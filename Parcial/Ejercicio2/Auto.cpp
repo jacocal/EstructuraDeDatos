ca86682dfc1e5b5da4de468826968d66b92057f4
jacocal <cacovolt@gmail.com>
2015-02-19 14:23:27 -0600

    Create Auto.cpp

---
+++ Parcial/Ejercicio2/Auto.cpp
@@ -0,0 +1,12 @@
+#include "Auto.h"
+
+Auto::Auto(){
+    
+}
+
+Auto::Auto(int cod, string mar, string modelo, float kilo){
+    cod = codigo;
+    mar = marca;
+    modelo = modeloA;
+    kilo = kilometraje;
+}
