0cef981e4f8be4728c958219a9c29e2883625ecd
jacocal <cacovolt@gmail.com>
2015-02-19 14:27:14 -0600

    Create Moto.h

---
+++ Parcial/Ejercicio2/Moto.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Moto{
+public:
+    int codigo;
+    string marca;
+    string modeloA;
+    float kilometraje;
+    string cilindraje;
+    
+    
+public:
+    Moto();
+    Moto(int, string, string, float, string);
+    
+};
