0c2b562402ee2b2957ff25314a2c496d46f8eee1
jacocal <cacovolt@gmail.com>
2015-02-19 14:23:47 -0600

    Create Auto.h

---
+++ Parcial/Ejercicio2/Auto.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Auto{
+public:
+    int codigo;
+    string marca;
+    string modeloA;
+    float kilometraje;
+    
+public:
+    Auto();
+    Auto(int, string, string, float);
+    
+};
