945dc6db8d0a75c8366bb77b15f894bf2d404c3d
jacocal <cacovolt@gmail.com>
2015-02-19 14:26:57 -0600

    Create Moto.cpp

---
+++ Parcial/Ejercicio2/Moto.cpp
@@ -0,0 +1,13 @@
+#include "Moto.h"
+
+Moto::Moto(){
+    
+}
+
+Moto::Moto(int cod, string mar, string modelo, float kilo, string cilin){
+    cod = codigo;
+    mar = marca;
+    modelo = modeloA;
+    kilo = kilometraje;
+    cilin = cilindraje;
+}
