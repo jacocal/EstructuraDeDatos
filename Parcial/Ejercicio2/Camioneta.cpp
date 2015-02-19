6b250c92a213777b1e4f600a94d67a2c50f676de
jacocal <cacovolt@gmail.com>
2015-02-19 14:24:39 -0600

    Create Camioneta.cpp

---
+++ Parcial/Ejercicio2/Camioneta.cpp
@@ -0,0 +1,13 @@
+#include "Camioneta.h"
+
+Camioneta::Camioneta(){
+    
+}
+
+Camioneta::Camioneta(int cod, string mar, string modelo, float kilo, string trax){
+    cod = codigo;
+    mar = marca;
+    modelo = modeloA;
+    kilo = kilometraje;
+    trax = traccion;
+}
