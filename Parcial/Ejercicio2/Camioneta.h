5f03514ada255e815f1216d3e38747cab84845eb
jacocal <cacovolt@gmail.com>
2015-02-19 14:24:58 -0600

    Create Camioneta.h

---
+++ Parcial/Ejercicio2/Camioneta.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Camioneta{
+public:
+    int codigo;
+    string marca;
+    string modeloA;
+    float kilometraje;
+    string traccion;
+    
+public:
+    Camioneta();
+    Camioneta(int, string, string, float, string);
+    
+};
