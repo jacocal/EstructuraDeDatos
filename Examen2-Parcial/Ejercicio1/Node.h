53a283f795e7f91f61ceca96cb44f1ad07c9edd6
jacocal <cacovolt@gmail.com>
2015-04-09 13:36:53 -0500

    Create Node.h

---
+++ Examen2-Parcial/Ejercicio1/Node.h
@@ -0,0 +1,57 @@
+#ifndef LinkedList_Node_h
+#define LinkedList_Node_h
+
+#include <iostream>
+
+template <class T>
+class Node {
+    T info;
+    Node<T> * next = nullptr;
+    
+public:
+    
+    /* Constructores */
+    Node() { }
+    Node(const T & _info) : info (_info) { }
+    Node(const Node<T> & );
+    
+    /* Destructor */
+    virtual ~Node();
+    
+    T getInfo() const { return info; }
+    void setInfo(const T & value) { info = value; }
+    
+    Node<T> * getNext() const { return next; }
+    void setNext(Node<T> * value) { next = value; }
+    
+    template <typename Tn>
+    friend std::ostream & operator << (std::ostream & os, const Node<Tn>  & node);
+};
+
+template <class T>
+Node<T>::Node(const Node<T> & other)
+{
+    info = other.info;
+    next = other.next;
+}
+
+template <class T>
+Node<T>::~Node()
+{
+    info.~T();
+    next = nullptr;
+}
+
+
+template <class T>
+std::ostream & operator << (std::ostream & os, const Node<T>  & node)
+{
+    os << node.info;
+    
+    return os;
+}
+
+
+
+
+#endif
